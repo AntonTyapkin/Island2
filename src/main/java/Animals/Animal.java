a4dc581b19a2724faea7aa03db487b24630b4266
Nikolay Gabaraev <gargul41@gmail.com>
2022-04-29 18:12:02 +0300

    Create Main.java Create Animal.java

---
+++ src/main/java/Animals/Animal.java
@@ -0,0 +1,15 @@
+package Animals;
+
+/**
+ * @author Nikolay Gabaraev
+ * @created 29.04.2022
+ * @project JavaRush_Module-2
+ */
+public abstract class Animal {
+
+    public abstract void eat();
+
+    public abstract void reproduction();
+
+    public abstract void setDirection();
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Nikolay Gabaraev
+ * @created 29.04.2022
+ * @project JavaRush_Module-2
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
